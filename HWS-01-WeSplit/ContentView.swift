b80ad6d9dd9ba872e460e1c8366ff0ebb797455c
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-03-22 17:25:07 +0100

    Initial Commit

---
+++ HWS-01-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-01-WeSplit
+//
+//  Created by Manuel Casocavallo on 22/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-01-WeSplit/HWS_01_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_01_WeSplitApp.swift
+//  HWS-01-WeSplit
+//
+//  Created by Manuel Casocavallo on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_01_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
